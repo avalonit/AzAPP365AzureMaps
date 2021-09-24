fc5d453524d98ec06d3ca35b03b0e469e5c53d68
alberto <alberto.valenti@varprime.com>
2021-09-24 11:39:48 +0200

    AA

--- Classes/ConnectorConfig.cs
+++ Classes/ConnectorConfig.cs
@@ -16,18 +16,31 @@ namespace azureapp.app365
                 BcWebServiceCustUrl = config["bcWebServiceCustUrl"];
                 BcWebServiceCompany = config["bcWebServiceCompany"];
 
+                NavWebServiceCompany = config["navWebServiceCompany"];
+                NavWebServiceUser = config["navWebServiceUser"];
+                NavWebServicePassword = config["navWebServicePassword"];
+                NavWebServiceDomain = config["navWebServiceDomain"];
+                NavWebServiceMainUrl = config["navWebServiceMainUrl"];
+
                 AzureMapKey = config["azureMapKey"];
                 AzureMapEndpoint = config["azureMapEndpoint"];
             }
         }
 
-        public String BcWebServiceUser;
-        public String BcWebServicePassword;
-        public String BcWebServiceMainUrl;
-        public String BcWebServiceCustUrl;
-        public String BcWebServiceCompany;
-        public String AzureMapKey;
-        public String AzureMapEndpoint;
+        public string BcWebServiceUser;
+        public string BcWebServicePassword;
+        public string BcWebServiceMainUrl;
+        public string BcWebServiceCustUrl;
+        public string BcWebServiceCompany;
+
+        public string NavWebServiceCompany;
+        public string NavWebServiceUser;
+        public string NavWebServicePassword;
+        public string NavWebServiceDomain;
+        public string NavWebServiceMainUrl;
+
+        public string AzureMapKey;
+        public string AzureMapEndpoint;
 
     }
 }
